3a6b3b22f27c0170218609395214f9348e203aa3
Keerthana <keerthiram2612@gmail.com>
2024-11-05 20:29:57 +0530

    fixing the logo and created the navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Navbarstyle.css"
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className="container">
+        <div className="row">
+            <div className="col-md-4">
+            <div className="logo">
+            <i class="fa-solid fa-basket-shopping fa-2x">Groco</i>
+            </div>
+            </div>
+            <div className="col-md-4">
+
+            </div>
+            <div className="col-md-4">
+
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
