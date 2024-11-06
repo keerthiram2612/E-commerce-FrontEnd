a51c5e9728c34eba0ce2604b20e35da4cb72a73a
Keerthana <keerthiram2612@gmail.com>
2024-11-06 20:37:40 +0530

    created the list in the navbar of routes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,16 +6,25 @@ export default function Navbar() {
     <div className='navbar'>
       <div className="container">
         <div className="row">
-            <div className="col-md-4">
+            <div className="col-md-2">
             <div className="logo">
             <i class="fa-solid fa-basket-shopping fa-2x">Groco</i>
             </div>
             </div>
-            <div className="col-md-4">
-
-            </div>
-            <div className="col-md-4">
-
+            <div className="col-md-10">
+              <div className="navbar">
+                <ul className='route'>
+                 <a href=""><li>Home</li></a>
+                 <a href=""><li>Features</li></a>
+                 <a href=""><li>Products</li></a>
+                 <a href=""><li>Categories</li></a>
+                 <a href=""><li>Review</li></a>
+                 <a href=""><li>Blogs</li></a>
+                 <a href=''><li>Login</li></a>
+                 <i class="fa-solid fa-magnifying-glass fa-2x"></i>
+                 <i class="fa-solid fa-user fa-2x"></i>
+                </ul>
+              </div>
             </div>
         </div>
       </div>
