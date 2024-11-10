fc2996e895337c36dcfd52600981579515795c06
Keerthana <keerthiram2612@gmail.com>
2024-11-10 19:38:13 +0530

    making the image with responsive

--- src/Components/Homecontent.jsx
+++ src/Components/Homecontent.jsx
@@ -4,11 +4,20 @@ export default function Homecontent() {
   return (
     <div>
       <div className="home-image">
-        <div className="bg-image"></div>
+        <div className="bg-image text-center">
+        <div className="text">
+        <h1>Fresh and <span>Organic </span>Products For You</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam optio, velit 
+           accusamus corporis placeat natus laudantium dolores blanditiis
+            minus distinction.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem error ipsum .
+          </p>
+          <button>Shop Now</button>
+        </div>
+        </div>
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <div className="home">
+              <div className="home text-center">
                 
               </div>
             </div>
