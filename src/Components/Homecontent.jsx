62cd71f0397d0a4b9ec1197644e927982b7fc4c0
Keerthana <keerthiram2612@gmail.com>
2024-11-10 19:06:20 +0530

    fixing the image

--- src/Components/Homecontent.jsx
+++ src/Components/Homecontent.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import "./Homestyle.css"
 export default function Homecontent() {
   return (
     <div>
-      
+      <div className="home-image">
+        <div className="bg-image"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="home">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
